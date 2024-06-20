e5882acac18a2be1eff17ec567953975ba428ce8
Kadidia Niakate <Kadidia.NKT0@gmail.com>
2024-06-20 22:19:55 +0200

    v-5

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -7,13 +7,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/program', name: 'program_')]
 Class ProgramController extends AbstractController
 {
-    #[Route('/', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
     }
+
+    #[Route('/{id}', requirements: ['id' => '\d+'], methods: ['GET'], name: 'show')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [ 'id' => $id,]);
+    }
+
 }
